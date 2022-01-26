93ba7e6ec052bc7cea83bbfec8d544bd1ee8e5b9
shychen <shychen@xiaoe-tech.com>
2022-01-26 14:15:06 +0800

    feat:加载不到配置文件

--- pkg/setting/setting.go
+++ pkg/setting/setting.go
@@ -22,7 +22,10 @@ func init()  {
 	var err error
 	Cfg, err = ini.Load("conf/app.ini")
 	if err != nil {
-		log.Fatalf("Fail to parse 'conf/app.ini': %v", err)
+		Cfg, err = ini.Load("/data/gopath/go-lang/conf/app.ini")
+	}
+	if err != nil {
+		log.Fatalf("Fail to parse 'conf/app.ini or /data/gopath/go-lang/conf/app.ini': %v", err)
 	}
 	LoadBase()
 	LoadServer()
