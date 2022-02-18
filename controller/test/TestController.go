c8a9ad322b2e5293589e2db0f0e71e11a7c667d2
shychen <shychen@xiaoe-tech.com>
2022-02-18 16:29:43 +0800

    feat:新增图集

--- controller/test/TestController.go
+++ controller/test/TestController.go
@@ -3,10 +3,12 @@ package test
 import (
 	"fmt"
 	"gin-blog/controller/upload"
+	"gin-blog/models/resource"
 	"gin-blog/pkg/app"
 	"gin-blog/pkg/e"
 	"gin-blog/pkg/util"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 	"path"
 	"strings"
 )
@@ -48,4 +50,44 @@ func TestThumb(c *gin.Context)  {
 	}
 	app.OK(c)
 	return
+}
+
+// TestPortSecond 测试缩略图效果
+func TestPortSecond(c *gin.Context)  {
+	photoList := resource.GetAboutPhotos(42)
+	var r resource.PhotosList
+	err := c.ShouldBindBodyWith(&r, binding.JSON)
+	if err != nil {
+		return 
+	}
+	var  oldArr []interface{}
+	var  newArr []interface{}
+	var  newInsert []interface{}
+	var  delArr []interface{}
+	for _,value := range photoList{
+		oldArr = append(oldArr,value.Id)
+	}
+
+	for _,value := range r.PhotosArr{
+		if value.Id > 0 {
+			newArr = append(newArr,value.Id)
+		} else {
+			newInsert = append(newInsert,value)
+		}
+	}
+	// 求出需要删除的图片
+	for _,value := range oldArr{
+		if !util.InArray(newArr,value) {
+			delArr = append(delArr,value)
+		}
+	}
+	// 执行删除操作
+	if len(delArr) > 0 {
+		resource.DelPhotosList(delArr)
+	}
+
+	// 执行新增操作
+	if len(newInsert) > 0 {
+		resource.BatchInsertPhoto(newInsert)
+	}
 }
--- models/resource/photos.go
+++ models/resource/photos.go
@@ -3,9 +3,12 @@ package resource
 import (
 	"encoding/json"
 	"fmt"
+	"gin-blog/pkg/util"
+	jsonTime "gin-blog/pkg/util/json"
 )
 
 const PhotosTableName = "t_go_photos"
+const PhotosListTableName = "t_go_photos_list"
 
 // PhotosData 创建图片结构体
 type PhotosData struct {
@@ -16,6 +19,31 @@ type PhotosData struct {
 	Thumb	  string `json:"thumb"`
 	FileName  string `json:"file_name" validate:"required"`
 	IsTop     int 	 `json:"is_top"`
+}
+
+type PhotosDetail struct {
+	Id 		  	  int    `json:"id"`
+	Title     	  string `json:"title" validate:"required"`
+	Summary   	  string `json:"summary"`
+	Url 	  	  string `json:"url" validate:"required"`
+	Thumb	  	  string `json:"thumb"`
+	FileName  	  string `json:"file_name" validate:"required"`
+	IsTop     	  int 	 `json:"is_top"`
+	CreatedAt     jsonTime.JSONTime 	 `json:"created_at"`
+}
+
+type PhotosList struct {
+	PhotosArr []Photo `json:"changePhotoArr"`
+}
+
+type Photo struct {
+	Id 		  	  int    `json:"id"`
+	Uid           int 	 `json:"uid"`
+	Status        string `json:"status"`
+	ResourceId    int 	 `json:"resource_id"`
+	Url 	  	  string `json:"url"`
+	Thumb	  	  string `json:"thumb"`
+	FileName  	  string `json:"file_name"`
 }
 
 // CreatPhotoRecord 创建记录
@@ -80,8 +108,8 @@ func DeletePhotoRecord(id int) bool {
 }
 
 // GetPhotoDetail 获取图片详情
-func GetPhotoDetail(id int) (PhotosData,error) {
-	var r PhotosData
+func GetPhotoDetail(id int) (PhotosDetail,error) {
+	var r PhotosDetail
 	err := db.Table(PhotosTableName).Where("id = ?",id).Where("is_delete = ?", 1).First(&r)
 	if err.Error != nil {
 		return r,err.Error
@@ -96,4 +124,28 @@ func UpdatePhotoDetail(id int, data PhotosData) bool  {
 		return false
 	}
 	return true
+}
+
+// BatchInsertPhoto 批量插入角色和权限
+func BatchInsertPhoto (data []interface{}) bool {
+	sql := util.GetBranchInsertSql(data,PhotosListTableName)
+	db.Exec(sql)
+	if db.Error != nil {
+		return false
+	}
+	return true
+}
+
+// GetAboutPhotos 去查询与之关联的系列的图片
+func GetAboutPhotos(resourceId int) []Photo {
+	var r []Photo
+	db.Table(PhotosListTableName).Select("id,uid,url,thumb,file_name,status,resource_id").
+		Where("resource_id = ?",resourceId).
+		Where("is_delete = ?",1).Find(&r)
+	return r
+}
+
+// DelPhotosList 批量更新删除状态
+func DelPhotosList(data []interface{}) {
+	db.Table(PhotosListTableName).Where("id in (?)", data).Updates(map[string]interface{}{"is_delete": 2})
 }
